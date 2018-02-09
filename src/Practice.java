5856edb556eea056843340b2a45eb869a25faf4f
patrickatcw <pj8392@yahoo.com>
2018-02-09 11:14:09 -0500

    2nd commit for Practice class

--- src/Practice.java
+++ src/Practice.java
@@ -2,12 +2,18 @@ public class Practice {
 
     public static void main(String[] args) {
 
-        int score = 5000;
+        int score = 4000;
 
 
         if (score == 5000) {
 
             System.out.println("Your score is " + score);
+
+
+        }else{
+
+
+            System.out.println("Bad Score");
 
         }
     }
