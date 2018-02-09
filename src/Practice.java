64ce99bce6498b5efc4160d4999cba891f8388f4
patrickatcw <pj8392@yahoo.com>
2018-02-09 11:10:22 -0500

    first commit for Practice class

---
+++ src/Practice.java
@@ -0,0 +1,14 @@
+public class Practice {
+
+    public static void main(String[] args) {
+
+        int score = 5000;
+
+
+        if (score == 5000) {
+
+            System.out.println("Your score is " + score);
+
+        }
+    }
+}
