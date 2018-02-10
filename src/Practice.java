d9add42962924289be79f74663cffee57d764bf2
patrickatcw <pj8392@yahoo.com>
2018-02-10 11:26:54 -0500

    same code block

--- src/Practice.java
+++ src/Practice.java
@@ -2,19 +2,17 @@ public class Practice {
 
     public static void main(String[] args) {
 
-        int score = 4000;
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
 
 
         if (score == 5000) {
 
-            System.out.println("Your score is " + score);
-
-
-        }else{
-
-
-            System.out.println("Bad Score");
-
+            System.out.println("Your score was 5000");
+            System.out.println("This was executed");
+            //both executed b/c both within code block
         }
     }
 }
