aa723bea66aac388e775d74c06bfc9f4414d405b
patrickatcw <pj8392@yahoo.com>
2018-02-10 12:18:34 -0500

    scope challenge

--- src/Practice.java
+++ src/Practice.java
@@ -17,9 +17,28 @@ public class Practice {
 
         }
 
-        //int savedFinalScore = finalscore;
-        //would result in error b/c outside of codeblock and finalScore already
-        //used in execution
+        /*Challenge
+        - print out a second score on the screen with the following
+        - score set to 1000
+        - levelCompleted set to 0
+        - bonus set to 200
+        but make sure the first print out still displays as well
+        */
+
+        score = 10000;   //same variable names but changed values, redefined
+        levelCompleted = 8;
+        bonus = 200;
+
+        if (gameOver) {
+
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+            //int finalScore is 11600, boolean confirmed
+
+        }
+
+
+
 
     }
 }
